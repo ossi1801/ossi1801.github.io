4921a14e3b52a832c76b8f8834d141693ca4177e
ossi1801 <ossihakki@gmail.com>
2025-09-18 22:28:01 +0300

    stylings

---
+++ static/js/script.js
@@ -0,0 +1,28 @@
+var canvas = document.querySelector('canvas'),
+ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+//var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZ';
+var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZあいうえおかきくけこがぎぐげごさしすせそざじずぜぞたちつてとだぢづでどなにぬねのはひふへほば びぶべぼぱぴぷぺぽまみむめもやゆよらりるれろわをんABCDEFGHIJKLMNOPQRSTUVXYZあいうえおかきくけこがぎぐげごさしすせそざじずぜぞたちつてとだぢづでどなにぬねのはひふへほば びぶべぼぱぴぷぺぽまみむめもやゆよらりるれろわをんABCDEFGHIJKLMNOPQRSTUVXYZあいうえおかきくけこがぎぐげごさしすせそざじずぜぞたちつてとだぢづでどなにぬねのはひふへほば びぶべぼぱぴぷぺぽまみむめもやゆよらりるれろわをん';
+letters = letters.split('');
+var fontSize = 10,
+columns = canvas.width / fontSize;
+var drops = [];
+for (var i = 0; i < columns; i++) {
+    drops[i] = 1;
+}
+function draw() {
+    ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < drops.length; i++) {
+        var text = letters[Math.floor(Math.random() * letters.length)];
+        ctx.fillStyle = '#0f0';
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+        drops[i]++;
+        if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+            drops[i] = 0;
+        }
+    }
+}
+// Loop the animation
+setInterval(draw, 33);
